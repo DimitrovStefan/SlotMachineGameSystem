54eb488d84acfb97b78beb795c7b535264c48112
stefan9764 <stefan100297@gmail.com>
2024-11-07 12:24:26 +0200

    MainMenu component added

---
+++ SlotMachineGameSystem/Components/MainMenuComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SlotMachineGameSystem.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+
+    }
+}
